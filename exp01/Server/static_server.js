866b3e2ec782b6267703c5e74a214db8e6a97745
Raja D G <dgraja@ssdiblr.sharp.co.in>
2019-03-01 19:44:04 +0530

    exp01: serving static content using express

--- exp01/Server/static_server.js
+++ exp01/Server/static_server.js
@@ -1,7 +1,13 @@
 // Import the express module
 var express = require('express');
+// Import serve-static module to serve static contents
+var serveStatic = require('serve-static');
+var path = require('path');
 // Create an object of express
 var app = express();
+var staticRoot = path.join(path.dirname(__dirname), 'webstatic');
+console.log(staticRoot);
+app.use('/web', serveStatic(staticRoot));
 // HTTP GET handler for root url (/) will return "Hello world!" message
 app.get('/', function(req, res){
     res.send("Hello world!");
