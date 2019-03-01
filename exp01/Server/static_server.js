6562170c924d11a37dbd384e70b089eb3207e816
Raja D G <dgraja@ssdiblr.sharp.co.in>
2019-03-01 20:35:14 +0530

    exp01: Add URL Building function

--- exp01/Server/static_server.js
+++ exp01/Server/static_server.js
@@ -1,16 +1,17 @@
+/*jshint esversion: 6 */
+
 // Import the express module
 var express = require('express');
 
+var path = require('path');
+
 
 // ------------ Static Content ----------
-// Import serve-static module to serve static contents
-var serveStatic = require('serve-static');
-var path = require('path');
 // Create an object of express
 var app = express();
 var staticRoot = path.join(path.dirname(__dirname), 'webstatic');
-console.log(staticRoot);
-app.use('/web', serveStatic(staticRoot));
+console.log(`Serving static files from ${staticRoot}`);
+app.use('/web', express.static(staticRoot));
 
 
 // ------------ Basic HTTP GET handler ----------
@@ -30,6 +31,24 @@ api_about.get('/', function(req, res) {
 // Add the router for relative url /api/about
 app.use('/api/about', api_about);
 
+
+// -------- Dynamic Urls ---------------------
+var api_user = express.Router();
+api_user.get('/:userid', function(req, res){
+    res.send(`<h1>User requested is : ${req.params.userid}</h1>`);
+});
+app.use('/api/user', api_user);
+// Example: http://localhost:8080/api/user/tester
+
+
+// -------- Multiple params sample ---------------------
+var api_product_find = express.Router();
+api_product_find.get('/:category/:pagesize([0-9]{1,3})/:pagenumber([0-9]{1,3})', function(req, res){
+    res.send(`<h1>The Category requested is : ${req.params.category}</h1>
+    <h2>page: ${req.params.pagenumber}, size: ${req.params.pagesize}</h2>`);
+});
+app.use('/api/products', api_product_find);
+// Example: http://localhost:8080/api/products/mobiles/100/1
 
 // Listen in the target port
 app.listen(8080);
