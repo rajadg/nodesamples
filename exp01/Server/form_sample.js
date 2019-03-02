cfd8e12b53fd66a0a78b72cc530a6a3937462340
Raja D G <dgraja@ssdiblr.sharp.co.in>
2019-03-02 19:47:57 +0530

    exp01: Form handling

--- exp01/Server/form_sample.js
+++ exp01/Server/form_sample.js
@@ -3,6 +3,7 @@
 // Import the express module
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 
 // A static website
 var app = express();
@@ -10,6 +11,19 @@ var staticRoot = path.join(path.dirname(__dirname), 'webstatic');
 console.log(`Serving static files from ${staticRoot}`);
 app.use('/web', express.static(staticRoot));
 
+// Form handling
+// Add body parser middleware to parse application/xwww-urlencoded post
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Receive the posted form data
+app.post('/web/userInfo', function(req, res){
+    console.log(req.body);
+    var receivedData = {
+        name: req.body.name,
+        age: req.body.age
+    };
+    res.send(`Posted data: ${JSON.stringify(receivedData, space=' ')}`);
+ });
 
 // listen at port 8080
 app.listen(8080);
